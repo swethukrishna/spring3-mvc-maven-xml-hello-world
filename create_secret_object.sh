52912df45f1c5ae2173d84b2226ee288b628904f
Ubuntu <ubuntu@ip-172-31-42-187.ap-south-1.compute.internal>
2022-01-30 16:28:23 +0000

    adding deployment

---
+++ create_secret_object.sh
@@ -0,0 +1,20 @@
+# RUN me where kubectl is available,& make sure to replace account,region etc
+#
+ACCOUNT=041221872505
+REGION=ap-south-1
+SECRET_NAME=${REGION}-ecr-registry
+EMAIL=ybmadhu404@gmail.com
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+#
+# Create or replace registry secret
+#
+
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+ --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+ --docker-username=AWS \
+ --docker-password="${TOKEN}" \
+ --docker-email="${EMAIL}"
+
