b0295a4816e5bf94ca663a560b013cab77ea097d
Ubuntu <ubuntu@ip-172-31-5-3.ap-south-1.compute.internal>
2022-06-29 02:58:15 +0000

    added deployment

---
+++ create_secret_ecr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ACCOUNT=849846292328
+REGION=ap-south-1
+SECRET_NAME=${REGION}-ecr-registry
+EMAIL=ybmadhu404@gmail.com
+#
+# Fetch token (which will expire in 12 hours)
+#
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+#
+# Create or replace registry secret
+#
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+ --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+ --docker-username=AWS \
+ --docker-password="${TOKEN}" \
+ --docker-email="${EMAIL}"
+
